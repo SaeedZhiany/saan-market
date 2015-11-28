f97116d345a696273f2f8520cb7582bac5970e7d
Anooshik <vartanian.anooshik@gmail.com>
2015-11-29 01:32:17 +0330

    اضافه کردن یک تمپلیت کلی برای پروژه اضافه کردن دکمه "صفحه سان  " در منوی بالایی لینک کردن دکمه "صفحه سان" به یک صفحه html ساده شروع کار

---
+++ saan-market/Controllers/SaanController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace saan_market.Controllers
+{
+    public class SaanController : Controller
+    {
+        // GET: Saan
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+   /*     public string Index()
+        {
+            return "این ایندکس ه";
+        }
+
+        public string Welcome()
+        {
+
+            return "صفحه اول";
+        }*/
+    }
+}
--- saan-market/Properties/AssemblyInfo.cs
+++ saan-market/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("saan_market")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("26a5ba27-5ba9-4e20-a307-4ed88ab68ef9")]
+[assembly: Guid("b2d3f0a2-6b25-4b9d-a03a-fb4a62ca904a")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
