448397ad5a6a979edadb245cf8740903ea2459a5
Anooshik <vartanian.anooshik@gmail.com>
2015-11-29 21:40:15 +0330

    اضافه کردن صفحه درباره ما و تماس با ما لینک تصاویر درست کار میکنند.

---
+++ saan-market/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(saan_market.Startup))]
+namespace saan_market
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
