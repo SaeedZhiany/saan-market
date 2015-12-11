597a884751992567cc149ce2574b28ac2824de6f
saeed-zh <gsilyp@gmail.com>
2015-12-11 19:43:46 +0330

    اتمام مورد کاربری لاگ این ادمین

--- saan-market/Controllers/AccountingController.cs
+++ saan-market/Controllers/AccountingController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using saan_market.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +10,54 @@ namespace saan_market.Controllers
 {
     public class AccountingController : Controller
     {
-        
+        public ActionResult Adminstrator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adminstrator(string username, string password)
+        {
+            try
+            {
+                using (DatabaseEntities context = new DatabaseEntities())
+                {
+                    var query = (from user in context.Users
+                                 where user.username == username
+                                 select new { user.password, user.isAdmin });
+                    if (query.Count() == 1)
+                    {
+                        var result = query.First();
+                        SHA1 sha = new SHA1CryptoServiceProvider();
+                        byte[] passBytes = System.Text.Encoding.UTF8.GetBytes(password);
+                        byte[] passHash = sha.ComputeHash(passBytes);
+                        if (result.password.SequenceEqual(passHash))
+                        {
+                            if (result.isAdmin)
+                            {
+                                return RedirectToAction("DefineProduct", "Product");
+                            }
+                            else
+                            {
+                                ModelState.AddModelError("", "شما یک کاربر عادی هستید. برای دسترسی به حساب خود از طریق لینک ورود در صفحه اصلی اقدام کنید.");
+                                return View();
+                            }
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "نام کاربری یا رمز ورود اشتباه است.");
+                            return View();
+                        }
+
+                    }
+
+                    throw new Exception("نام کاربری یا رمز عبور اشتباه است.");
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
