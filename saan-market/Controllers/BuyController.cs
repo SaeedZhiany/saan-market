13f6ab4ee322e79608491eb46f9976e69df60cc4
Anooshik <vartanian.anooshik@gmail.com>
2015-12-21 23:06:01 +0330

    درست شد بازبینی خرید!

--- saan-market/Controllers/BuyController.cs
+++ saan-market/Controllers/BuyController.cs
@@ -7,27 +7,25 @@ using System.Web.Mvc;
 namespace saan_market.Controllers
 {
     public class BuyController : Controller
-    {/*
-        public ActionResult checkOutStep1()
+    {
+        public ActionResult checkout_1()
+        {
+            return View();
+        }
+        public ActionResult checkout_2()
+        {
+            return View();
+        }
+        public ActionResult checkout_3()
+        {
+            return View();
+        }
+        public ActionResult checkout_4()
         {
             return View();
         }
 
-        public ActionResult checkOutStep2()
-        {
-            return View();
-        }
 
-        public ActionResult checkOutStep3()
-        {
-            return View();
-        }
-
-        public ActionResult checkOutStep4()
-        {
-            return View();
-        }
-        */
         public ActionResult factor()
         {
             return View();
